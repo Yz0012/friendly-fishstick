219497c1b1809c5066b8b6118e508955e388f4f2
Y_z00 <yyzz00@foxmail.com>
2023-11-05 13:47:03 +0800

    测试

--- yyzz/Impl.java
+++ yyzz/Impl.java
@@ -5,7 +5,7 @@ public class Impl {
         Linkimpl<String> link = new Linkimpl<String>();
         link.add("null");
         link.add("fucking deep");
-        link.add("NIHAO");
+        link.add("HELLO");
         link.add("ZTXD");
         System.out.println(link.getCount());
     }
