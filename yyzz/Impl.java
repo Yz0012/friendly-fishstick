7339d3c5a8fa3a9c7f36a5a720f99619ed7fc02a
Y_z00 <yyzz00@foxmail.com>
2023-11-05 12:20:35 +0800

    modified:   yyzz/Impl.java 	yyzz/friendly-fishstick/

--- yyzz/Impl.java
+++ yyzz/Impl.java
@@ -5,7 +5,7 @@ public class Impl {
         Linkimpl<String> link = new Linkimpl<String>();
         link.add("null");
         link.add("fucking deep");
-        link.add("TESTE");
+        link.add("TESTESTS");
         link.add("ZTXD");
         System.out.println(link.getCount());
     }
